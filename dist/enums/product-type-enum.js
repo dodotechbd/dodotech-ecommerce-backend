5cb6338581dc702e3e81505574c7218655b7ff0e
MShafiMS <shahedali734@gmail.com>
2023-12-24 16:10:30 +0600

    updated for deploy

---
+++ dist/enums/product-type-enum.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductType = void 0;
+/* eslint-disable no-unused-vars */
+var ProductType;
+(function (ProductType) {
+    ProductType["PHONE"] = "Phone";
+    ProductType["COMPUTERS"] = "Computers";
+    ProductType["SMARTWATCH"] = "Smart Watche";
+    ProductType["CAMERA"] = "Camera";
+    ProductType["HEADPHONE"] = "Headphone";
+    ProductType["GAMING"] = "Gaming";
+})(ProductType = exports.ProductType || (exports.ProductType = {}));
